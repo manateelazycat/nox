e711dad0f966734972299f8cf95a46b360e5229f
Andy Stewart <lazycat.manatee@gmail.com>
2020-03-28 16:33:59 +0800

    Add copyright info.

--- nox.el
+++ nox.el
@@ -1,7 +1,87 @@
-;;; nox.el --- Client for Language Server Protocol (LSP) servers  -*- lexical-binding: t; -*-
+;;; nox.el --- Nox is LSP Client for Emacs   -*- lexical-binding: t; -*-
 
-;;; Code:
+;; Filename: nox.el
+;; Description: Nox is LSP Client for Emacs
+;; Author: João Távora <joaotavora@gmail.com>
+;;         Andy Stewart <lazycat.manatee@gmail.com>
+;; Maintainer: Andy Stewart <lazycat.manatee@gmail.com>
+;; Copyright (C) 2018, João Távora, all rights reserved.
+;; Copyright (C) 2020, Andy Stewart, all rights reserved.
+;; Created: 2020-03-28 16:27:25
+;; Version: 0.1
+;; Last-Updated: 2020-03-28 16:27:25
+;;           By: Andy Stewart
+;; URL: http://www.emacswiki.org/emacs/download/nox.el
+;; Keywords:
+;; Compatibility: GNU Emacs 26.3
+;;
+;; Features that might be required by this library:
+;;
+;;
+;;
 
+;;; This file is NOT part of GNU Emacs
+
+;;; License
+;;
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program; see the file COPYING.  If not, write to
+;; the Free Software Foundation, Inc., 51 Franklin Street, Fifth
+;; Floor, Boston, MA 02110-1301, USA.
+
+;;; Commentary:
+;;
+;; Nox is LSP Client for Emacs
+;;
+
+;;; Installation:
+;;
+;; Put nox.el to your load-path.
+;; The load-path is usually ~/elisp/.
+;; It's set in your ~/.emacs like this:
+;; (add-to-list 'load-path (expand-file-name "~/elisp"))
+;;
+;; And the following to your ~/.emacs startup file.
+;;
+;; (require 'nox)
+;;
+;; No need more.
+
+;;; Customize:
+;;
+;;
+;;
+;; All of the above can customize by:
+;;      M-x customize-group RET nox RET
+;;
+
+;;; Change log:
+;;
+;; 2020/03/28
+;;      * First released.
+;;
+
+;;; Acknowledgements:
+;;
+;;
+;;
+
+;;; TODO
+;;
+;;
+;;
+
+;;; Require
 (require 'json)
 (require 'cl-lib)
 (require 'project)
@@ -20,6 +100,7 @@
 (defvar company-backends) ; forward-declare, but don't require company
 (defvar company-tooltip-align-annotations)
 
+;;; Code:
 
 ;;; User tweakable stuff
 (defgroup nox nil
